b214817b672f8dde87fdc6a02e25724f87a6e5aa
BlumaBerkowicz <BlumaBerkowicz@users.noreply.github.com>
2025-05-25 12:47:03 +0000

    deploy: b95ac230c3acbd64fb624ba3abbafb1e9238b6ec

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TFUT6ULE.mjs";import"./chunk-L3DD4EOH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-DFG5JDWU.mjs";import"./chunk-L3DD4EOH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
